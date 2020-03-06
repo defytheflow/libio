0dd7443f23220a44592ae2059013102a191ddd15
Artyom Danilov <defytheflow@gmail.com>
2020-03-06 14:17:51 +0300

    'cleario' - created 'examples' directory. Wrote an example for 'get_string' function.

---
+++ cleario/examples/example_get_string.c
@@ -0,0 +1,24 @@
+#include <cleario.h>
+
+#include <stdio.h>
+
+#define NUM_WORDS 5
+#define MAX_WORD  256
+
+int main(void)
+{
+        int i, j, len;
+        char words[NUM_WORDS][MAX_WORD];
+
+        printf("Enter %d words.\n", NUM_WORDS);
+        for (i = 0; i < NUM_WORDS; ++i) {
+                if ((len = get_string(words[i], MAX_WORD)) == 0)
+                        break;
+        }
+
+        printf("Entered words:\n");
+        for (j = 0; j < i; ++j)
+                printf("%s\n", words[j]);
+
+        return 0;
+}
